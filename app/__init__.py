0198a22a2122eeba392121fafbdafac12fbe185b
ruifortescv <ruimiguelfortes@gmail.com>
2025-11-17 18:17:07 +0000

    instalação cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import config
@@ -8,8 +9,10 @@ migrate = Migrate()
 
 def create_app(config_mode='development'):
     app = Flask(__name__)
+    CORS(app)
+   
     app.config.from_object(config[config_mode])    
- 
+   
     dbt.init_app(app)
     migrate.init_app(app, dbt)
 

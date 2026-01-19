7e95ce5d216dfda95de338fdfc2f4b3e25f43b43
ruifortescv <ruimiguelfortes@gmail.com>
2026-01-19 15:42:10 -0100

    testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from app import create_app,dbt
 
 @pytest.fixture
 def app():
-    app = create_app("testing")
+    app = create_app("development")
   
     with app.app_context():
         dbt.create_all()

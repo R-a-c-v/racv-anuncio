848b16453e63e84fb91f18cd5e37d3fe93ea9d89
ruifortescv <ruimiguelfortes@gmail.com>
2026-01-22 09:31:47 -0100

    testes

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ migrate = Migrate()
 
 def create_app(config_mode):
     app = Flask(__name__)
-    CORS(app)
     app.config.from_object(config[config_mode])    
+    CORS(app)
  
     dbt.init_app(app)
     migrate.init_app(app, dbt)

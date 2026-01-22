2e7469483d572b3d10e8b319531e0e0b2eafeb3b
ruifortescv <ruimiguelfortes@gmail.com>
2026-01-22 09:40:59 -0100

    testes

--- app/app.py
+++ app/app.py
@@ -2,13 +2,13 @@ import os
 
 from  app import create_app 
 
-config_mode = os.getenv("FLASK_CONFIG", "development")
-app = create_app(config_mode)
-#123
+ 
 #123
 #123
 from  app.anuncios.routes.urls  import *
 
 if __name__=='__main__':
+     config_mode = os.getenv("FLASK_CONFIG", "development")
+     app = create_app(config_mode)
      app.run()
 #end-if
